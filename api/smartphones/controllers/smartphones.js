e5c946cb20773127269ba586760eb9081c9a2909
LeadHUsp <bodvinukr@gmail.com>
2020-03-20 16:44:25 +0300

    add filter func and route to smartphones

--- api/smartphones/controllers/smartphones.js
+++ api/smartphones/controllers/smartphones.js
@@ -7,40 +7,23 @@
 const { sanitizeEntity } = require("strapi-utils");
 module.exports = {
   async find(ctx) {
-    /*  let filter_values = {
-      display_sizes: [],
-      series: [],
-      memory_models: [],
-      brands: []
-    };
-    function onlyUnique(value, index, self) {
-      return self.indexOf(value) === index;
-      } */
-
     let entities;
     let perPage = ctx.query._limit || 3;
     let currentPage = ctx.query.page || 1;
     ctx.query._limit = perPage * ctx.query.page;
     if (ctx.query.page) {
       delete ctx.query.page;
+      delete ctx.query.limit;
     }
     if (ctx.query._q) {
       entities = await strapi.services.smartphones.search(ctx.query);
     } else {
-      entities = await strapi.services.smartphones.find();
+      entities = await strapi.services.smartphones.find(ctx.query.params);
     }
 
     let res = entities.map(entity => {
       return sanitizeEntity(entity, { model: strapi.models.smartphones });
     });
-    /*  let filter = res.map(item => {
-      filter_values.display_sizes.push(item.display_size);
-      filter_values.series.push(item.series);
-      filter_values.memory_models.push(item.internal_memory);
-      filter_values.brands.push(item.brand);
-      return filter_values;
-    });
-    console.log(filter); */
 
     let total = res.length;
     let totalPages = Math.ceil(total / perPage);
@@ -56,5 +39,35 @@ module.exports = {
       let start = end - perPage;
       return res.slice(start, end);
     }
+  },
+  async filter() {
+    let filter_values = {
+      display_sizes: [],
+      series: [],
+      memory_models: [],
+      brands: []
+    };
+    function onlyUnique(value, index, self) {
+      return self.indexOf(value) === index;
+    }
+    let entities = await strapi.services.smartphones.find();
+
+    let res = entities.map(entity => {
+      return sanitizeEntity(entity, { model: strapi.models.smartphones });
+    });
+    res.map(item => {
+      filter_values.display_sizes.push(item.display_size);
+      filter_values.series.push(item.series);
+      filter_values.memory_models.push(item.internal_memory);
+      filter_values.brands.push(item.brand);
+      return filter_values;
+    });
+    let filter = {
+      display_sizes: filter_values.display_sizes.filter(onlyUnique),
+      series: filter_values.series.filter(onlyUnique),
+      memory_models: filter_values.memory_models.filter(onlyUnique),
+      brands: filter_values.brands.filter(onlyUnique)
+    };
+    return filter;
   }
 };
